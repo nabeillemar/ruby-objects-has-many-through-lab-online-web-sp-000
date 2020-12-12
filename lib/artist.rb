9892531b35e2ece4a9f941cf42564e56205466b7
Nabeil Lemar <nabeillemar@gmail.com>
2020-12-12 04:29:58 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+class Artist 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  =begin
+  
+  def songs 
+    @@all.select do |song|
+      song.collect == self 
+    end 
+  end 
+  
+  =end
+  
+  
+end 
